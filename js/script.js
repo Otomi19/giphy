6be63893482d5a616cbeb9d130640473ea1ebf55
Otomi19 <oo6661@thewcs.org>
2020-05-14 21:59:48 +0000

    Done Alot

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+   let search = $("#search-term").val();
+  fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&rating=pg&api_key=dc6zaTOxFJmzC`)
+  .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+         $(".display").append(`<img src=${data.data[Math.floor(Math.random() * 10)].images['downsized'].url
+}>`);
+      });
 });
 
